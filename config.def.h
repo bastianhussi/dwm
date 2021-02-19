30882315f95638e541bcf81dfb5e3391d3fc4fb4
Bastian Hussi <bastian@ipfso.de>
2021-02-19 19:37:05 +0100

    Added rules and modified shortcuts

--- config.def.h
+++ config.def.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 6;        /* gaps between windows */
+static const unsigned int borderpx  = 8;       /* border pixel of windows */
+static const unsigned int gappx     = 12;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -11,8 +11,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=15" };
+static const char dmenufont[]       = "monospace:size=15";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -32,32 +32,53 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class         instance title                  tags mask  isfloating isterminal noswallow monitor */
+	{ "st",              NULL,    NULL,                  1 << 0,    0,         1,         0,        -1 },
+	{ "Firefox",         NULL,    NULL,                  1 << 1,    0,         0,        -1,        -1 },
+	{ "Tor Browser",     NULL,    NULL,                  1 << 1,    1,         0,         0,        -1 },
+	{ "Thunderbird",     NULL,    NULL,                  1 << 1,    0,         0,         0,        -1 },
+	{ "Zathura",         NULL,    NULL,                  1 << 2,    0,         0,         0,        -1 },
+	{ "Emacs",           NULL,    NULL,                  1 << 3,    0,         0,         0,        -1 },
+	{ "VSCodium",        NULL,    NULL,                  1 << 3,    0,         0,         0,        -1 },
+	{ "Insomnia",        NULL,    NULL,                  1 << 3,    0,         0,         0,        -1 },
+	{ "Fritzing",        NULL,    NULL,                  1 << 3,    0,         0,         0,        -1 },
+	{ "Virt-manager",    NULL,    NULL,                  1 << 4,    1,         0,         0,        -1 },
+	{ "Gimp",            NULL,    NULL,                  1 << 5,    1,         0,         0,        -1 },
+	{ "krita",           NULL,    NULL,                  1 << 5,    0,         0,         0,        -1 },
+	{ "Inkscape",        NULL,    NULL,                  1 << 5,    0,         0,         0,        -1 },
+	{ "vlc",             NULL,    NULL,                  1 << 6,    0,         0,         0,        -1 },
+	{ "Kodi",            NULL,    NULL,                  1 << 6,    0,         0,         0,        -1 },
+	{ "Microsoft Teams", NULL,    NULL,                  1 << 7,    0,         0,         0,        -1 },
+	{ "discord",         NULL,    NULL,                  1 << 7,    0,         0,         0,        -1 },
+	{ "Steam",           NULL,    NULL,                  1 << 8,    0,         0,         0,        -1 },
+	{ "Steam",           NULL,    "Friends List",        1 << 8,    1,         0,         0,        -1 },
+	{ "Steam",           NULL,    "Screenshot Uploader", 1 << 8,    1,         0,         0,        -1 },
+	{ "Steam",           NULL,    "Steam - News",        1 << 8,    1,         0,         0,        -1 },
+	{ NULL,              NULL,    "Event Tester",        0,         0,         0,         1,        -1 }, /* xev */
+	{ "KeePassXC",       NULL,    NULL,                  0,         1,         0,         0,        -1 },
+	{ "Solaar",          NULL,    NULL,                  0,         1,         0,         0,        -1 },
+	{ "Timeshift-gtk",   NULL,    NULL,                  0,         1,         0,         0,        -1 },
 };
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
  	{ "[@]",      spiral },
  	{ "[\\]",     dwindle },
 	{ "|M|",      centeredmaster },
 	{ ">M>",      centeredfloatingmaster },
+	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ NULL,       NULL },
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -74,7 +95,7 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -87,19 +108,12 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[3]} },
-	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[4]} },
-	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[5]} },
-	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[6]} },
-	{ MODKEY|ControlMask,		    XK_comma,  cyclelayout,    {.i = -1 } },
-	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
+	{ MODKEY,                       XK_w,      killclient,     {0} },
+	{ MODKEY,                       XK_comma,  cyclelayout,    {.i = -1 } },
+	{ MODKEY,                       XK_period, cyclelayout,    {.i = +1 } },
+	{ MODKEY,                       XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_f,      togglefullscr,  {0} },
+	{ MODKEY,                       XK_u,      focusurgent,    {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
@@ -116,7 +130,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY,                       XK_u,      focusurgent,    {0} },
 };
 
 /* button definitions */
